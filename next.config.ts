17529840ff32bfa78fb4624b7a01377bce3aaa7a
PHlushkov <pavelgluskov264@gmail.com>
2025-04-21 17:49:50 +0300

    Refactor middleware and configuration for internationalization setup

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,23 @@
-// import {NextConfig} from 'next';
-// import createNextIntlPlugin from 'next-intl/plugin';
-
-// const withNextIntl = createNextIntlPlugin({
-//   experimental: {
-//     createMessagesDeclaration: './messages/en.json'
-//   }
-// });
-
-// const config: NextConfig = {
-//   i18n: {
-//     locales: ['de', 'en'],
-//     defaultLocale: 'en',
-//     domains: [
-//       {
-//         domain: 'store.qpart.com.ua',
-//         defaultLocale: 'en',
-//         locales: ['de', 'en']
-//       }
-//     ]
-//   }
-// };
-
-// export default withNextIntl(config);
-
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin({
+  experimental: {
+    createMessagesDeclaration: './messages/en.json'
+  }
+});
 
-const config: NextConfig = {};
+const config: NextConfig = {
+  basePath: ''
+};
 
 export default withNextIntl(config);
+
+// import {NextConfig} from 'next';
+// import createNextIntlPlugin from 'next-intl/plugin';
+
+// const withNextIntl = createNextIntlPlugin();
+
+// const config: NextConfig = {};
+
+// export default withNextIntl(config);
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,35 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
+// import createMiddleware from 'next-intl/middleware';
+// import {routing} from './i18n/routing';
 
-export default createMiddleware(routing);
+// export default createMiddleware(routing);
+
+// export const config = {
+//   // Match all pathnames except for
+//   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+//   // - … the ones containing a dot (e.g. `favicon.ico`)
+//   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+// };
+
+import createMiddleware from 'next-intl/middleware';
+
+export default createMiddleware({
+  locales: ['ua', 'de', 'en'],
+  defaultLocale: 'ua',
+  domains: [
+    {
+      domain: 'store.qpart.com.ua',
+      defaultLocale: 'ua',
+      locales: ['ua']
+    },
+    {
+      domain: 'de.store.qpart.com.ua',
+      defaultLocale: 'de',
+      locales: ['de']
+    }
+    // ...
+  ]
+});
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
+  matcher: ['/((?!_next).*)']
 };
