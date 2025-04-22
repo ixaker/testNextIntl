2f7df06111b039cf924ed215a483e6f02c736512
PHlushkov <pavelgluskov264@gmail.com>
2025-04-22 13:12:35 +0300

    refactor: enhance logging in middleware for better debugging and clarity

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,17 @@ import type { NextRequest } from "next/server";
 
 // Это минимальный middleware, который ничего не делает — просто пропускает запрос
 export function middleware(req: NextRequest) {
+  console.log("Поавли в middleware");
+
   // Можно залогировать, чтобы убедиться, что middleware сработал
   const { pathname } = req.nextUrl;
+
+  console.log("pathname", pathname, req.nextUrl);
 
   const pathnameIsMissingLocale = !/^\/(en|ru|uk)(\/|$)/.test(pathname);
 
   if (pathnameIsMissingLocale) {
+    console.log("попали в pathnameIsMissingLocale", pathnameIsMissingLocale);
     const acceptLang = req.headers.get("accept-language");
     const browserLang = acceptLang?.split(",")[0].split("-")[0] || "en";
     const redirectLocale = ["en", "ru", "uk"].includes(browserLang)
@@ -16,9 +21,11 @@ export function middleware(req: NextRequest) {
       : "en";
 
     const redirectPath = `/${redirectLocale}${pathname}`;
+
+    console.log("redirectPath", redirectPath);
     return NextResponse.redirect(redirectPath);
   }
-
+  console.log("next");
   return NextResponse.next();
 }
 
@@ -26,5 +33,5 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  matcher: "/",
 };
