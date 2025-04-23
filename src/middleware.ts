ff69e91781ba8886388f79c74b0b15d4873bd82e
PHlushkov <pavelgluskov264@gmail.com>
2025-04-23 12:13:02 +0300

    refactor: remove unused URL handling and streamline data extraction in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,22 @@ import { NextResponse, NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const nextUrl = request.nextUrl;
+  // const nextUrl = request.nextUrl;
+  const headers = request.headers;
+
+  // const data = {
+  //   href: nextUrl.href,
+  //   origin: nextUrl.origin,
+  //   host: nextUrl.host,
+  //   hostname: nextUrl.hostname,
+  // };
 
   const data = {
-    href: nextUrl.href,
-    origin: nextUrl.origin,
-    host: nextUrl.host,
-    hostname: nextUrl.hostname,
+    href: headers.get("host"),
+    origin: headers.get("x-forwarded-host"),
   };
+
+  console.log("data", data);
 
   return NextResponse.json(data);
 }
